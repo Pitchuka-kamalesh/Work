6ef812857cb464235680bb04338c94257843c6ca
kamalesh <kamalesh@ascustech.in>
2024-12-17 13:52:13 +0530

    17/dec/2024 today Task i have done regarding the Automation Testing

---
+++ src/test/java/org/ascus/AuthBidi.java
@@ -0,0 +1,23 @@
+package org.ascus;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.edge.EdgeDriver;
+import java.time.Duration;
+
+
+public class AuthBidi {
+    public static void main(String[] args)  {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.bing.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(2));
+        WebElement searchBar = driver.findElement(By.cssSelector("textarea[name='q']"));
+        searchBar.sendKeys("cricketScore");
+        searchBar.submit();
+        driver.navigate().refresh();
+        CricketPage cricket = new CricketPage(driver);
+        System.out.println(cricket.firstTeamScore());
+        System.out.println(cricket.secondTeamScore());
+    }
+
+}
